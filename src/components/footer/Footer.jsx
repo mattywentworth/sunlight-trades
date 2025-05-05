b0cd730dd463a4000afac75aa51b41287e782b7d
mattywentworth <wentworthmm@gmail.com>
2025-05-05 19:41:43 -0400

    Update Footer css module to make it show properly on Netlify site

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import styles from './Footer.module.css';
 export const Footer = () => {
 
     return (
-        <footer>
+        <footer className={styles.footer}>
             <div>Sunlight Trades</div>
             <div>Disclaimer: Sunlight Trades is not a financial advisor. Nothing on this website should be taken as financial advice.</div>
         </footer>
