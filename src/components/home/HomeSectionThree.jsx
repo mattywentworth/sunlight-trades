71e77f5b15d0479a8f41488d16a22c1496f32dee
mattywentworth <wentworthmm@gmail.com>
2025-05-05 19:33:19 -0400

    Add styling update to homepage

--- src/components/home/HomeSectionThree.jsx
+++ src/components/home/HomeSectionThree.jsx
@@ -6,14 +6,16 @@ export const HomeSectionThree = () => {
 
     return (
         <div className={styles.container}>
-            <div className={styles.textOne}>Track your buy, hold, sell, and watch decisions.</div>
-            <div className={styles.textOne}>Confront how psychology helps and harms how you invest.</div>
+            <div className={styles.textOne}>Don't just track your investments. Track your <span>decisions</span>.</div>
+            <div className={styles.textOne}>Confront how psychology <span>helps</span> and <span>harms</span> how you invest.</div>
             {/*<div className={styles.textOne}>Will you pretend to understand how emotions impact the way you invest your money?</div>
             <div className={styles.textTwo}>Or will you hold yourself accountable and make more reliable decisions?</div>
             <div className={styles.textTwo}>Use Sunlight Trades to document your thoughts and behaviors and learn from them...</div>*/}
-            <div className={styles.textThree}>Because Sunlight is the best disinfectant.</div>
+            
             <div className={styles.spacer}></div>
             <Link className={styles.link} to='/login'>Sign Up For Free</Link>
+            <div className={styles.spacer}></div>
+            <h3 className={styles.textThree}>Because Sunlight is the best disinfectant.</h3>
         </div>
     )
 }
